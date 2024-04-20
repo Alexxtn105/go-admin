2bce343cdee7337c4b06ad24deeb3195bd5eb11d
Alexxtn105 <netmann105@gmail.com>
2024-04-20 15:53:06 +0300

    MySql connected

--- main.go
+++ main.go
@@ -1,13 +1,34 @@
 package main
 
 import (
-	"log"
-
+	"fmt"
 	"github.com/gofiber/fiber/v3"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+// Зависимости:
+// Fiber (web framework):
+// go get github.com/gofiber/fiber/v3
+
+// GORM - для связи с БД mysql
+
+//go get -u gorm.io/gorm
+// go get -u gorm.io/driver/mysql
+
+import (
+	"log"
 )
 
 func main() {
-	// Initialize a new Fiber app
+	// подключаемся к БД
+	db, err := gorm.Open(mysql.Open("root:Sulubun205!@/go-admin"), &gorm.Config{})
+	if err != nil {
+		panic("Could not connect to the database")
+	}
+	fmt.Println("Database: ", db)
+
+	// Инициализируем новое приложение Fiber
 	app := fiber.New()
 
 	// Define a route for the GET method on the root path '/'
