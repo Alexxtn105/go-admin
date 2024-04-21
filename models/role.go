83b3797d045516fef660ab7cc4f649bb3c8685d2
Alexxtn105 <netmann105@gmail.com>
2024-04-21 20:15:57 +0300

    Добавил таблицу для отношения многие-ко-многим для ролей и разрешений.

--- models/role.go
+++ models/role.go
@@ -3,4 +3,10 @@ package models
 type Role struct {
 	Id   uint   `json:"id""`
 	Name string `json:"name"`
+
+	// Роль может иметь несколько разрешений.
+	// Чтобы установить отношение  "многие ко многим",
+	// необходимо создать таблицу role_permissions с помощью gorm с соответствующмим отношением (many2many):
+
+	Permissions []Permission `json:"permission" gorm:"many2many: role_permissions"`
 }
